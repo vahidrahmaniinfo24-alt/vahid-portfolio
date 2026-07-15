59a124535ef703059508cb9ed005352b0664b8a5
[dyad] <git@dyad.sh>
2026-07-15 18:27:44 +0200

    [dyad] Using local vr.jpg as homepage profile photo - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/portfolio/ProfilePhoto.tsx
+++ src/components/portfolio/ProfilePhoto.tsx
@@ -1,11 +1,12 @@
 import { cn } from "@/lib/utils";
+import profileImage from "../../../img/vr.jpg";
 
 const ProfilePhoto = ({ className, src }: { className?: string; src?: string }) => {
   return (
     <div className={cn("relative mx-auto h-32 w-32 sm:h-40 sm:w-40", className)}>
       <div className="absolute -inset-1 rounded-full bg-portfolio-accent/20 blur-md"></div>
       <img
-        src={src || "file:///C:/Users/DCI-Student/dyad-apps/dreamy-starfish-snap/img/vr.jpg"}
+        src={src || profileImage}
         alt="Vahid Rahmani"
         className="relative h-full w-full rounded-full border-2 border-portfolio-accent/50 object-cover"
       />
